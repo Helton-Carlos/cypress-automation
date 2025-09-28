3b3b536bc1f13f35811a2ae1da40c7f0b29a09b7
Helton Brito <heltoncarlospa@hotmail.com>
2025-09-28 07:28:27 -0300

    feat: test page register user

---
+++ cypress/e2e/cadastrarusuarios.cy.js
@@ -0,0 +1,27 @@
+describe('Page cadastrarusuarios', () => {
+  beforeEach(function () {
+    cy.fixture('users').as('usuarios');
+
+    cy.visit('https://front.serverest.dev/cadastrarusuarios');
+  });
+
+  it('register with error', function () {
+    cy.get('[data-testid="nome"]').type(this.usuarios.adminerror.name);
+    cy.get('[data-testid="email"]').type(this.usuarios.adminerror.email);
+    cy.get('[data-testid="password"]').type(this.usuarios.adminerror.password);
+
+    cy.get('[data-testid="cadastrar"]').click();
+
+    cy.get('.alert > :nth-child(2)');
+  });
+
+  it('register with success', function () {
+    cy.get('[data-testid="nome"]').type(this.usuarios.admin.name);
+    cy.get('[data-testid="email"]').type(this.usuarios.admin.email);
+    cy.get('[data-testid="password"]').type(this.usuarios.admin.password);
+
+    cy.get('[data-testid="cadastrar"]').click();
+
+    cy.url().should('include', '/home');
+  });
+});
